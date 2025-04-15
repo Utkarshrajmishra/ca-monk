36ddec21314dec97aaf6bc43d36cae91bb2844ca
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-04-15 12:47:31 +0530

    Add: Header

---
+++ src/component/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-12 shadow w-full font-inter flex items-center justify-center">
+      <p className="text-neutral-900">Sentence Construction</p>
+    </header>
+  );
+};
+
+export default Header;
